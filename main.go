75eb1c430b70ee34b15d0c90a5118b84739af31c
xiaowei <wei.xiao@525j.com.cn>
2018-01-29 18:08:33 +0800

    添加mysql驱动

--- main.go
+++ main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"ginWebDemo/api/v1"
-	//"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	//gin.SetMode(gin.ReleaseMode)
-	v1.SetupRounter().Run("8080")
+	gin.SetMode(gin.ReleaseMode)
+	v1.SetupRounter().Run("0.0.0.0:8080")
 }
